8ddf3c03d842c499ca1c41ec552c77b752defd8c
unknown <hello@jeffdelaney.me>
2019-05-13 21:18:38 -0700

    feat: topics screens

--- lib/screens/topics.dart
+++ lib/screens/topics.dart
@@ -1,14 +1,206 @@
 import 'package:flutter/material.dart';
+import '../services/services.dart';
 import '../shared/shared.dart';
+import '../screens/screens.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class TopicsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('topics'), backgroundColor: Colors.purple),
+    return FutureBuilder(
+      future: Global.topicsRef.getData(),
+      builder: (BuildContext context, AsyncSnapshot snap) {
+        if (snap.hasData) {
+          List<Topic> topics = snap.data;
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple,
+              title: Text('Topics'),
+              actions: [
+                IconButton(
+                  icon: Icon(FontAwesomeIcons.userCircle,
+                      color: Colors.pink[200]),
+                  onPressed: () => Navigator.pushNamed(context, '/profile'),
+                )
+              ],
+            ),
+            drawer: TopicDrawer(topics: snap.data),
+            body: GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20.0),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: topics.map((topic) => TopicItem(topic: topic)).toList(),
+            ),
+            bottomNavigationBar: AppBottomNav(),
+          );
+        } else {
+          return LoadingScreen();
+        }
+      },
+    );
+  }
+}
 
-      bottomNavigationBar: AppBottomNav(),
+class TopicItem extends StatelessWidget {
+  final Topic topic;
+  const TopicItem({Key key, this.topic}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Hero(
+        tag: topic.img,
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => TopicScreen(topic: topic),
+                ),
+              );
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  'assets/covers/${topic.img}',
+                  fit: BoxFit.contain,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10, right: 10),
+                        child: Text(
+                          topic.title,
+                          style: TextStyle(
+                              height: 1.5, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.fade,
+                          softWrap: false,
+                        ),
+                      ),
+                    ),
+                    // Text(topic.description)
+                  ],
+                ),
+                // )
+                TopicProgress(topic: topic),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TopicScreen extends StatelessWidget {
+  final Topic topic;
+
+  TopicScreen({this.topic});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: ListView(children: [
+        Hero(
+          tag: topic.img,
+          child: Image.asset('assets/covers/${topic.img}',
+              width: MediaQuery.of(context).size.width),
+        ),
+        Text(
+          topic.title,
+          style:
+              TextStyle(height: 2, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        QuizList(topic: topic)
+      ]),
+    );
+  }
+}
+
+class QuizList extends StatelessWidget {
+  final Topic topic;
+  QuizList({Key key, this.topic});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Column(
+        children: topic.quizzes.map((quiz) {
+      return Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        elevation: 4,
+        margin: EdgeInsets.all(4),
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => QuizScreen(quizId: quiz.id),
+              ),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: ListTile(
+              title: Text(
+                quiz.title,
+                style: Theme.of(context).textTheme.title,
+              ),
+              subtitle: Text(
+                quiz.description,
+                overflow: TextOverflow.fade,
+                style: Theme.of(context).textTheme.subhead,
+              ),
+              leading: QuizBadge(topic: topic, quizId: quiz.id),
+            ),
+          ),
+        ),
+      );
+    }).toList());
+  }
+}
+
+class TopicDrawer extends StatelessWidget {
+  final List<Topic> topics;
+  TopicDrawer({Key key, this.topics});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView.separated(
+          shrinkWrap: true,
+          itemCount: topics.length,
+          itemBuilder: (BuildContext context, int idx) {
+            Topic topic = topics[idx];
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 10, left: 10),
+                  child: Text(
+                    topic.title,
+                    // textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white70,
+                    ),
+                  ),
+                ),
+                QuizList(topic: topic)
+              ],
+            );
+          },
+          separatorBuilder: (BuildContext context, int idx) => Divider()),
     );
   }
 }
