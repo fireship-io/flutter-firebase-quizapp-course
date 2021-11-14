542402abb81187c3587dad8639bdc7a9e60200cc
Jeff Delaney <hello@fireship.io>
2021-11-13 20:23:15 -0700

    mv quiz badge

--- lib/shared/progress_bar.dart
+++ lib/shared/progress_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp/services/models.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
 class AnimatedProgressbar extends StatelessWidget {
@@ -55,25 +54,6 @@ class AnimatedProgressbar extends StatelessWidget {
   _colorGen(double value) {
     int rbg = (value * 255).toInt();
     return Colors.deepOrange.withGreen(rbg).withRed(255 - rbg);
-  }
-}
-
-class QuizBadge extends StatelessWidget {
-  final String quizId;
-  final Topic topic;
-
-  const QuizBadge({Key? key, required this.quizId, required this.topic})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Report report = Provider.of<Report>(context);
-    List completed = report.topics[topic.id] ?? [];
-    if (completed.contains(quizId)) {
-      return const Icon(FontAwesomeIcons.checkDouble, color: Colors.green);
-    } else {
-      return const Icon(FontAwesomeIcons.solidCircle, color: Colors.grey);
-    }
   }
 }
 
--- lib/topics/drawer.dart
+++ lib/topics/drawer.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:quizapp/quiz/quiz.dart';
 import 'package:quizapp/services/models.dart';
-import 'package:quizapp/shared/progress_bar.dart';
 
 
 class TopicDrawer extends StatelessWidget {
@@ -83,5 +84,24 @@ class QuizList extends StatelessWidget {
         },
       ).toList(),
     );
+  }
+}
+
+class QuizBadge extends StatelessWidget {
+  final String quizId;
+  final Topic topic;
+
+  const QuizBadge({Key? key, required this.quizId, required this.topic})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Report report = Provider.of<Report>(context);
+    List completed = report.topics[topic.id] ?? [];
+    if (completed.contains(quizId)) {
+      return const Icon(FontAwesomeIcons.checkDouble, color: Colors.green);
+    } else {
+      return const Icon(FontAwesomeIcons.solidCircle, color: Colors.grey);
+    }
   }
 }
